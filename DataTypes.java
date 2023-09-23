e5cb9ca91acc4ddbf035727a2a386611d85a7b24
Vipul Kumar <92398016+itsvipulkumar@users.noreply.github.com>
2023-09-23 10:32:03 +0530

    java 1

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String args[]) {
+        int int_data = 100; // instance varibale
+        double double_data = 1312.123;
+        char char_data = 'A';
+        float float_data=1.334f;
+        long long_data=3123464641252L;
+        short short_data=13;
+        byte byte_data=1;
+        Boolean boolean_data=false;
+
+        System.out.println("int data type : " + int_data);
+        System.out.println(" double_data type : " + double_data);
+        System.out.println("Char data type : " + char_data);
+        System.out.println(" float_data type : " + float_data);
+        System.out.println(" long_data type : " + long_data);
+        System.out.println(" short_data type : " + short_data);
+        System.out.println(" byte_data type : " + byte_data);
+        System.out.println(" boolean_data type : " + boolean_data);
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+
+    static int value=10; // static variable
+    public void mehtod()
+    {
+        int x=23; // local varibale
+        System.out.println(x);
+    }
+    public static void main(String args[])
+    {
+        int a=100; // instance varibale
+        
+        System.out.println(a);
+        System.out.println(value);
+    }
+}
