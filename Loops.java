3e78f2d17436d4d989992bb615fd5d77d4e45dd6
Vipul Kumar <92398016+itsvipulkumar@users.noreply.github.com>
2023-09-23 12:04:09 +0530

    java 3

---
+++ Loops.java
@@ -0,0 +1,30 @@
+public class Loops {
+    public static void main(String args[]) {
+        int n=10;
+
+        // will print from 1 to n;
+        // for loop
+    //    for(int i=1;i<=n;i++)
+    //    {
+    //     System.out.print(i+" ");
+    //    }
+
+       // while loop
+    //    int i=1;
+    //    while(i<=n)
+    //    {
+    //     System.out.println(i+" ");
+    //     i++;
+    //    }
+
+       //do while loop
+       int i=1;
+       do{
+        System.out.print(i+" ");
+        i++;
+       }while(i<=n);
+      
+
+
+    }
+}
---
+++ NewSwitchCase.java
@@ -0,0 +1,16 @@
+public class NewSwitchCase {
+    public static void main(String args[]) {
+        int n = 3;
+
+        switch (n) {
+            case 1->System.out.println("One");
+               
+            case 2->System.out.println("Two");
+                
+            case 3->System.out.println("Three");
+        
+            default->System.out.println("Other");
+              
+        }
+    }
+}
